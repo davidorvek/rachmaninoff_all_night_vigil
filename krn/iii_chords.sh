adabf89b3991a41401fd2220178f8a1149a2b148
David Orvek <davideo23@yahoo.com>
2019-02-06 11:03:37 -0500

    changed script

--- krn/iii_chords.sh
+++ krn/iii_chords.sh
@@ -1,6 +1,14 @@
 for file in *.krn
 do
-	key=$(extractx -f 1 $file | grep '\*.*:' | sed 's/[\*:]//g')
+	title=$(grep '!!!OTL@EN' $file | awk 'BEGIN{FS=":"}{print $2}' | sed 's/^[[:space:]]*//g')
+	key=$(extractx -f 1 $file | grep '\*.*:' | sed 's/[\*:mixdorphr]//g')
 	mediant=$(awk -v awk_key=$key '$1 == awk_key {print $2}' mediants.txt)
-	roots=$(sonority -r $file)
+	sonority -r $file | grep -v '[X=0-9\*\!rest]' >> all_chords.tmp
+	sonority -r $file | grep -v '[X=0-9\*\!rest]' | grep "$mediant" >> mediants.tmp
+	echo "..."
 done
+wc -l all_chords.tmp | awk '{print $1}' > number_of_chords.tmp
+wc -l mediants.tmp | awk '{print $1}' > number_of_mediants.tmp
+paste number_of_mediants.tmp number_of_chords.tmp > both.tmp
+awk '{total = $1 / $2}{print total}' both.tmp
+rm *.tmp
